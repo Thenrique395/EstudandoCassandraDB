f0b2f06547ab808d1a60b0d8edd29d9796ab4de0
Tiago <thenrique395@gmail.com>
2020-06-13 21:36:02 -0300

    primeiro Commit

---
+++ src/main/java/com/desafio/estudandoCassandraDB/EstudandoCassandraDbApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.estudandoCassandraDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoCassandraDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoCassandraDbApplication.class, args);
+	}
+
+}
